88d5f9d36fba7a37b5d6e65af0a6dafbe1210d04
Oliver Forral <oforral@mesd.k12.or.us>
2014-08-22 10:42:01 -0700

    Created person and thing entities

---
+++ src/Mesd/ManyToManyBundle/Entity/Person.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Mesd\ManyToManyBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Person
+ */
+class Person
+{
+    /**
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $name;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Person
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+}
